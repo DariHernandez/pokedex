3aadac3757131c9e61358d6116b5cf4c18255501
daridev <hernandezdarifrancisco@gmail.com>
2022-09-11 13:24:58 -0500

    fix gallery responsive

--- code/gallery.js
+++ code/gallery.js
@@ -1,10 +1,13 @@
 var splide = new Splide( '.splide', {
     type: 'loop'
-} );
+} )
 
-splide.mount();
+splide.mount()
 
-window.onresize = function () {
+window.onresize = gallery_responsive()
+window.onload = gallery_responsive ()
+
+function gallery_responsive () {
     if (window.matchMedia ("(min-width: 600px)").matches) {
         splide.options = {
             padding: '5rem'        
